720046d3219bca40afff592b232fe743b0b9bc80
deepakPococare <deepak.k@pococare.com>
2024-01-03 21:44:51 +0530

    Update : changed fonts and colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,10 +14,10 @@ function Hero() {
       </nav>
       <h1 className="head_text">
         Summarize Articles with <br />
-        <span className="orange_gradient">OpenAI GPT-4</span>
+        <span className="blue_gradient">OpenAI Chat GPT-4</span>
       </h1>
-      <h2 className="text-center  m-auto my-5 min-w-4	">
-          Simplify your reading with Summize, an open-source article summarizer
+      <h2 className="text-center  m-auto my-5 w-[300px] sm:min-w-[500px]">
+          Simplify your reading with Summerize, an open-source article summarizer
           that transforms lengthy articles into clear and concise summaries
         </h2>
     </header>
